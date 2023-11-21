042ee5570f5210a3e04825d31ee69aeeeaa8d091
Mark Lape <121615238+Poowd@users.noreply.github.com>
2023-11-22 02:51:26 +0800

    Sample Structure
    includes:
    HTML Structure
    DATABASE Connection
    DATABASE Query

--- build/index.php
+++ build/index.php
@@ -5,20 +5,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enrollment System</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/temp.css">
 </head>
 <body>
-    <?php 
-        function OpenCon() {
-            $dbhost = "localhost";
-            $dbuser = "root";
-            $dbpass = "1234";
-            $db = "example";
-            $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
-                return $conn;
-        }
-        function CloseCon($conn) {
-            $conn -> close();
-        }
-    ?>
+    <div>
+        <h1>List of Enrolled Students</h1>
+        <br>
+        <button>New</button>
+        <br><br>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Created At</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "enrollment_system";
+
+                    //create connection
+                    $connection = new \mysqli($servername, $username, $password, $dbname);
+                        //check connection 
+                        if ($connection->connect_error) {
+                            die("Connection failed: ". $connection->connect_error);
+                        }
+                    //read all row from database table
+                    $sql = "SELECT * FROM students";
+                    $result = $connection->query($sql);
+                        if (!$result) {
+                            die("Invalid query: ". $connection->error);
+                        }
+                        
+                    //read data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "
+                            <tr>
+                                <td>$row[id]</td>
+                                <td>$row[name]</td>
+                                <td>$row[email]</td>
+                                <td>$row[created_at]</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        ";
+                    }
+                ?>
+
+                
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
