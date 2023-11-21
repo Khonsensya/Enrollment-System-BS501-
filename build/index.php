6612580b62406b7b371d2628f10d75e1e0820e11
Mark Lape <121615238+Poowd@users.noreply.github.com>
2023-11-21 15:36:46 +0800

    Database Connection

--- build/index.php
+++ build/index.php
@@ -8,7 +8,17 @@
 </head>
 <body>
     <?php 
-        echo "Hello World";
+        function OpenCon() {
+            $dbhost = "localhost";
+            $dbuser = "root";
+            $dbpass = "1234";
+            $db = "example";
+            $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
+                return $conn;
+        }
+        function CloseCon($conn) {
+            $conn -> close();
+        }
     ?>
 </body>
 </html>
