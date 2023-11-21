a3ddec5777f4ccba470891b45ad7edac15681141
E. I. Conggas <69233742+emmanisaac@users.noreply.github.com>
2023-11-21 16:18:28 +0800

    started database on index.php

--- build/index.php
+++ build/index.php
@@ -9,7 +9,30 @@
 </head>
 
 <body>
+    <?php
+        #  This PHP code serves as the DATABASE portion of the Finals Project: School Enrollment System.
+        #
+        #  Code Collaborators:
+        #  1. CONGGAS, Emman Isaac D.
+        #  2. GEREÑA, James Bernard O.
 
+        //Setting parameters for MySQL/SSMS connection (Placeholder, may be modified).
+        $server = "localhost";
+        $username = "bs501_enroll";
+        $password = "admin";
+        $database = "enrollmentDB";
+
+        $connect = mysqli_connect($server, $username, $password);
+
+        //Check connection
+        if(!$connect)
+        {
+          die("Connection interrupted or lost! " . mysqli_connect_error());
+        }
+        
+        echo "Connection successful ! >(^ o ^)>";
+        mysqli_close();
+    ?>
 </body>
 
 </html>
