a7b38ab31be2547cb6cb0994e997e8667bda69dd
E. I. Conggas <69233742+emmanisaac@users.noreply.github.com>
2023-11-22 14:40:48 +0800

    Created query.sql

---
+++ build/query.sql
@@ -0,0 +1,29 @@
+--SQL QUERY
+
+--DATABASE AND TABLE CREATION
+CREATE DATABASE enrollmentDB;
+
+CREATE TABLE students(
+  student_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP 
+);
+
+--CRUD OPERATIONS
+
+--CREATING DATA ON TABLE
+INSERT INTO students(
+    "John Doe", "johndoe.123456@example.com"
+  );
+
+--READING OR RETURN OUTPUT
+SELECT * FROM students WHERE id = 1; -- <== Specify which ID to retrieve data (where n is the id number)
+
+--UPDATE DATA IN TABLE
+UPDATE students
+SET name = "Juan Dela Cruz", email = "delacruz.123456@example.com" ; -- <== Spcify which columns are to be updated (refer to CREATE TABLE portion)
+WHERE id = n -- <== Specify which ID to retrieve data (for specificity)
+
+--DELETING DATA IN TABLE
+DELETE FROM students WHERE id = 1 --<== Delete an entry in the table; use id (for specificity)
