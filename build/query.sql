1698f8aa9d3c4e93866762638ace0b116211447f
E. I. Conggas <69233742+emmanisaac@users.noreply.github.com>
2023-11-30 14:09:41 +0800

    Updated query.sql
    Added new table 'courses'

--- build/query.sql
+++ build/query.sql
@@ -1,14 +1,24 @@
 --SQL QUERY
 
 --DATABASE AND TABLE CREATION
+/*
 CREATE DATABASE enrollmentDB;
+*/
 
+/*
 CREATE TABLE students(
   student_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP 
+  course_id INT NOT NULL PRIMARY KEY REFERENCES course(course_name)
 );
+*/
+
+CREATE TABLE courses(
+  course_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  course_name VARCHAR(96) NOT NULL
+)
 
 --CRUD OPERATIONS
 
@@ -25,5 +35,5 @@ UPDATE students
 SET name = "Juan Dela Cruz", email = "delacruz.123456@example.com" ; -- <== Spcify which columns are to be updated (refer to CREATE TABLE portion)
 WHERE id = n -- <== Specify which ID to retrieve data (for specificity)
 
---DELETING DATA IN TABLE
+--DELETING DATA IN TABLE3
 DELETE FROM students WHERE id = 1 --<== Delete an entry in the table; use id (for specificity)
