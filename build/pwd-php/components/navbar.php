f7fdd410005790734ce9672a70309ead3c7dfd47
Yranimez <133177603+Yranimez@users.noreply.github.com>
2023-12-19 19:48:12 +0900

    Summary
    Changes for pushing
    		.creation of about.php, admission.php, contactus.php, enroll.php, Login.php, and programs.php
    		.php connecting to homepage.
    		.placeholder text in php's

--- build/pwd-php/components/navbar.php
+++ build/pwd-php/components/navbar.php
@@ -8,10 +8,10 @@
                 <h1>Luv U</h1>
             </div>
             <ul class="navlist">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Admission</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <div class="btn-1"><a href="#">Login</a></div>
+                <li><a href="./pwd-php/modules/about.php">About</a></li>
+                <li><a href="./pwd-php/modules/programs.php">Programs</a></li>
+                <li><a href="./pwd-php/modules/admission.php">Admission</a></li>
+                <li><a href="./pwd-php/modules/contactus.php">Contact Us</a></li>
+                <div class="btn-1"><a href="./pwd-php/modules/Login.php">Login</a></div>
             </ul>
         </nav>
---
+++ build/pwd-php/modules/enroll.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action= "">
+        <label for= "fname">First Name:</label>
+        <input type="text" name="fname" placeholder="Given Name"><br><br>
+        <label for= "lname">Last Name:</label>
+        <input type="text" name="lname" placeholder="Surname"><br><br>
+        <label for= "mname">Middle Name(optional):</label>
+        <input type="text" name="mname" placeholder="Middle Name"><br>
+        <p>Gender:</p>
+        <label for= "male">Male</label>
+        <input type="radio" id="male" name="male"><br><br>
+        <label for= "female">Female</label>
+        <input type="radio" id="female" name="female"><br><br>
+        <!--need ayusin-->
+        <label for= "age">Age:</label>
+		<input type= "text" name="age" maxlength="3"><br><br>
+        <label for= "email">Email Address:</label>
+        <input type="text" id="email" name="email"><br><br>
+        <!--need ayusin-->
+        <label for= "phone">Phone Number:*</label>
+        <input type="tel" name="phone" placeholder="09*********" maxlength="11" \d* required */><br><br>
+        <!--need ayusin-->
+        <label for= "password">Password:*</label>
+        <input type="text" required ><br><br>
+        <input type="submit" name="submit" value="Create Account">
+        
+</body>
+</html>
