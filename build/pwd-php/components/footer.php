2aaf310badc2a635d8eba8c206a742863aa22ab8
Mark Lape <121615238+Poowd@users.noreply.github.com>
2023-12-19 17:57:27 +0800

    Revisiong [Lape]

---
+++ build/pwd-php/components/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <p>© copyright. BS501. 2023</p>
+    <p>Hotdog.</p>
+</footer>
--- build/pwd-php/data.php
+++ build/pwd-php/data.php
@@ -1,23 +1,51 @@
 <?php 
     $_benefits = [
-        ['icon' => './imgs/walkman_icon/icon2.png', 
+        ['icon' => './walkman-imgs/icon/icon2.png', 
          'title' => 'Te amo', 
          'description' => 'The words “te amo” perfectly encapsulate the spirit of lovers lost in each other’s arms. It’s an informal pronoun that expresses real intimacy. Spanish speakers don’t toss “amo” around lightly—it’s reserved for the real deal.', ],
                     
-        ['icon' => './imgs/walkman_icon/icon2.png', 
+        ['icon' => './walkman-imgs/icon/icon2.png', 
          'title' => ' 愛してる (aishi teru)', 
          'description' => '“Love” is a very strong word in Japanese culture and expressions of love are not very common—apart from in television dramas.', ],
                     
-        ['icon' => './imgs/walkman_icon/icon2.png',  
+        ['icon' => './walkman-imgs/icon/icon2.png',  
          'title' => '사랑해 (saranghae)', 
          'description' => 'When you say, “Saranghae,” the answer you would be looking for would probably be “Judo sarang haeyo!” (I love you, too!)', ],
                     
-        ['icon' => './imgs/walkman_icon/icon2.png',  
-         'title' => 'Tagalog: Mahal kita', 
+        ['icon' => './walkman-imgs/icon/icon2.png',  
+         'title' => 'Mahal kita', 
          'description' => '“Mahal kita” is used no matter your gender or the gender of your significant other. Although it’s mostly spoken in a romantic context, the phrase is sometimes used to express love to family members.', ],
                     
-        ['icon' => './imgs/walkman_icon/icon2.png', 
-         'title' => 'Chinese: 我爱你 (wǒ ài nǐ)', 
+        ['icon' => './walkman-imgs/icon/icon2.png', 
+         'title' => '我爱你 (wǒ ài nǐ)', 
          'description' => 'The Chinese have a saying: Lovers’ hearts are linked together and always beat as one.', ],
+        ];
+?>
+
+<?php 
+    $_contributors = [
+        ['icon' => './walkman-imgs/contributors/lape.jpg', 
+         'name' => 'Lape', 
+         'contribution' => 'FrontEnd', ],
+                    
+        ['icon' => './walkman-imgs/icon/icon1.jpg', 
+         'name' => 'Malonzo', 
+         'contribution' => 'FrontEnd', ],
+                    
+        ['icon' => './walkman-imgs/icon/icon1.jpg',  
+         'name' => 'Repil', 
+         'contribution' => 'Database', ],
+                    
+        ['icon' => './walkman-imgs/icon/icon1.jpg',  
+         'name' => 'Gerena', 
+         'contribution' => 'Data Gathering', ],
+                     
+        ['icon' => './walkman-imgs/icon/icon1.jpg',  
+        'name' => 'Austria', 
+        'contribution' => 'Data Gathering', ],
+                     
+        ['icon' => './walkman-imgs/icon/icon1.jpg',  
+        'name' => 'Conggas', 
+        'contribution' => 'Data Gathering', ],
         ];
 ?>
