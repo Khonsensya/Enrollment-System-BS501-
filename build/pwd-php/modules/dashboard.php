76e1f59e787af469e11e9ce9b2dd75f139332fe0
Mark Lape <121615238+Poowd@users.noreply.github.com>
2023-12-21 22:44:00 +0800

    login [lape]

--- build/pwd-php/modules/dashboard.php
+++ build/pwd-php/modules/dashboard.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Luv U University</title>
-    <link rel="stylesheet" href="./pwd-css/style.css">
-    <link rel="stylesheet" href="./pwd-css/modules.css">
-    <link rel="stylesheet" href="./pwd-css/components.css">
-    <link rel="stylesheet" href="./pwd-css/responsive.css">
-    <link rel="stylesheet" href="./pwd-css/animation.css">
+    <link rel="stylesheet" href="/build/pwd-css/style.css">
+    <link rel="stylesheet" href="/build/pwd-css/modules.css">
+    <link rel="stylesheet" href="/build/pwd-css/components.css">
+    <link rel="stylesheet" href="/build/pwd-css/responsive.css">
+    <link rel="stylesheet" href="/build/pwd-css/animation.css">
 </head>
 <body>
     dashboard
