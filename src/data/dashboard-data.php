a42e9b91bc7f749a9ec5900c2722db29378ffa71
Mark Lape <121615238+Poowd@users.noreply.github.com>
2023-12-29 15:52:02 +0800

    29/12/2023 [Lape]

--- src/data/dashboard-data.php
+++ src/data/dashboard-data.php
@@ -160,32 +160,4 @@
          'address5' => 'Leyte'], 
         // popular ng provinces/regions. atleast 10-15
         ];
-		
-	//Main Dashboard	
-	date_default_timezone_set('Asia/Manila');
-	function getCurrentSemester() {
-		$currentYear = date('Y');
-		$nextYear = $currentYear + 1;
-		$currentMonth = date('n');
-
-		if ($currentMonth >= 9 || $currentMonth <= 1) { // September - December, January
-			return 'SY' . $currentYear . $nextYear . '-1T';
-		} 
-		elseif ($currentMonth >= 2 && $currentMonth <= 7) { // February - July
-			return 'SY' . $currentYear . '-2T';
-		} 
-		else {
-			return 'Semestral Break';
-		}
-	}
-
-	$totalStudents = 10; // Placeholder Number 
-	$currentUser = 'User Name'; // Placeholder Name 
-	$currentDateTime = date('Y-m-d H:i:s');
-	
-	$dashboard_info = [
-		['code' => 'total_students', 'info_heading' => 'Total Students' . '<br>' . $totalStudents], 
-		['code' => 'semester_info', 'info_heading' => 'Current Semester' . '<br>' . getCurrentSemester()],
-		['code' => 'welcome_user', 'info_heading' => 'Welcome, ' . $currentUser . '<br>' . $currentDateTime],
-	];
 ?>
