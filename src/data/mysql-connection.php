96e610810ebb47e1e8d39c30a70dd9785c1e73f3
Kho <136983937+Khonsensya@users.noreply.github.com>
2023-12-29 16:39:19 +0800

    Added comment for clarity
    Added comment in mysql-connection.php

--- src/data/mysql-connection.php
+++ src/data/mysql-connection.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "enrollment_system";
-//$port = 4306;
+//$port = 4306; If MySQL port is different
 
 //create connection
 $mysqli = new mysqli($servername, $username, $password, $database) or die("Connection failed: " . $mysqli->connect_error);
